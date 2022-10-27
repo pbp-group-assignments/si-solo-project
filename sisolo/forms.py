1695096666d6f2d46516ecf43e35996092419c08
rifqifarelmuhammad <rifqifarel222@gmail.com>
2022-10-27 18:51:20 +0700

    Minor update forms field

--- sisolo/forms.py
+++ sisolo/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class Register(forms.Form):
     namaLengkap =  forms.CharField(label = 'Nama Lengkap')
-    nomorTelepon = forms.IntegerField(label='Nomor Telepon')
+    nomorTelepon = forms.CharField(label='Nomor Telepon')
     alamat = forms.CharField(label = 'Alamat Tempat Tinggal')
