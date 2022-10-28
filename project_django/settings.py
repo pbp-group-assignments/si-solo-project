2e0c365fa5d39db2077279d37200f9d599b5d34f
Fabian <fabiandion12@gmail.com>
2022-10-28 13:30:54 +0700

    Specify root directory for user-uploaded media

--- project_django/settings.py
+++ project_django/settings.py
@@ -138,6 +138,12 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Base url to serve media files  
+MEDIA_URL = '/media/'  
+  
+# Path where media is stored  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
