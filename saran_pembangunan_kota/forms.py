97080f9e30f56f89b8c2da66239bc2f075ca0311
mathildadellanova <mathilda.dellanova@ui.ac.id>
2022-11-02 20:51:41 +0700

    fixing saran

--- saran_pembangunan_kota/forms.py
+++ saran_pembangunan_kota/forms.py
@@ -1,7 +1,8 @@
+from dataclasses import field
 from django import forms
 from saran_pembangunan_kota.models import Saran
 
-class FormSaran(forms.Form):
+class FormSaran(forms.ModelForm):
     class Meta:
         model = Saran
         fields = ['nama', 'email', 'saran_pembangunan']
