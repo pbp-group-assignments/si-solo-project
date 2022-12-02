e1b46a5df34a2427f76535058f504a2639be754f
rifqifarelmuhammad <rifqifarel222@gmail.com>
2022-12-02 13:26:07 +0700

    mini update for endpoint

--- sisolo/views.py
+++ sisolo/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from sisolo.decorators import admin_only
-
 from sisolo.forms import Register
 from sisolo.models import User
 
