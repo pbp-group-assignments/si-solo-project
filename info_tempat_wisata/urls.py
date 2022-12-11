c26b56460f9ed2e9507949e8a318111c0e4e0cb0
Jasmine Indira Wibowo <103538319+Jasmineindira@users.noreply.github.com>
2022-12-11 15:59:55 +0700

    update

--- info_tempat_wisata/urls.py
+++ info_tempat_wisata/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', show_tempat_wisata, name = "show_tempat_wisata"),
     path('manage-json/', manage_wisata_json, name = "manage_wisata_json"),
     path('add-deskripsi-wisata', add_deskripsi_wisata, name='add_deskripsi_wisata'),
-    path('delete-deskripsi-wisata', delete_deskripsi_wisata, name='delete_deskripsi_wisata')
+    path('delete-deskripsi-wisata', delete_deskripsi_wisata, name='delete_deskripsi_wisata'),
+    path('add-wisata-mobile', add_wisata_mobile, name='add_wisata_mobile'),
 ]
